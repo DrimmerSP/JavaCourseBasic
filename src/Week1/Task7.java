084a8e9d56444e2d15b79caf74b22d36234eaaa1
Drimmer <drimmer88@gmail.com>
2022-12-10 16:12:52 +0300

    Initial commit

---
+++ src/Week1/Task7.java
@@ -0,0 +1,21 @@
+package Week1;
+
+import java.util.Scanner;
+/*
+    Дано двузначное число.
+    Вывести сначала первую цифру (единицы), затем правую (десятки)
+
+    Входные данные: 78
+    Выходные: 7
+              8
+ */
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+        int first = number / 10;
+        int second = number % 10;
+
+        System.out.println(first + " || " + second);
+    }
+}
