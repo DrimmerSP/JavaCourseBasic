8a013735d2f0faa501cc41f0090ce0992e910bad
Drimmer <drimmer88@gmail.com>
2022-12-28 19:36:14 +0300

    Home learn

---
+++ src/HomeWork3/coursesWork/workLearn7.java
@@ -0,0 +1,18 @@
+package HomeWork3.coursesWork;
+
+import java.util.Scanner;
+
+public class workLearn7 {
+    public static void main(String[ ] args) {
+        String s;
+        char letter_choice;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Введите латинскую букву от A до E: ");
+            s = input.nextLine();
+            letter_choice = s.charAt(0);;
+        } while (letter_choice < 'A' || letter_choice > 'E');
+    }
+}
