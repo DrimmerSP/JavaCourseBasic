c4721c18170079dff9ab88df65039b883f5c6a44
Drimmer <drimmer88@gmail.com>
2023-01-18 22:56:01 +0300

    Week6 vebinare

---
+++ src/week6/Task2.java
@@ -0,0 +1,49 @@
+package week6;
+/*
+На вход подается число N — ширина и высота матрицы.
+Необходимо заполнить матрицу 1 и 0 в виде шахматной доски.
+Нулевой элемент должен быть 0.
+
+Входные данные
+3
+Выходные данные
+0 1 0
+1 0 1
+0 1 0
+
+Входные данные
+4
+Выходные данные
+0 1 0 1
+1 0 1 0
+0 1 0 1
+1 0 1 0
+   */
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0){
+                    a[i][j] = 0;
+                }else {
+                    a[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
+
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
---
+++ src/week6/Task7.java
@@ -0,0 +1,39 @@
+package week6;
+/*
+Найдем факториал числа n рекурсивно.
+На вход - n
+ */
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            res = res * i;
+        }
+        System.out.println("Factorial (FOR LOOP): " + res);
+        System.out.println("Factorial (factorial): " + factorial(n));
+
+    }
+
+    // 3 -> 3*(factorial(2))  <- вернулась 1 и выполнилось действие (3*2) = 6
+    // 2 -> 2*(factorial(1))  <- вернулась 1 и выполнилось действие (2*1) = 2
+    // 1 -> 1
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static int factorialTail(int n, int result) {
+        if (n <= 1) {
+            return result;
+        }
+        return n * factorialTail(n - 1, n * result);
+    }
+}
