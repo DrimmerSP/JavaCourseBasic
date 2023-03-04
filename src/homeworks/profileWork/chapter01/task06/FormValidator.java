8e77f2eda72012854a9ee510327a7b01b0d5f25b
Drimmer <drimmer88@gmail.com>
2023-03-05 00:26:00 +0300

    ДЗ_1 задача 6

---
+++ src/homeworks/profileWork/chapter01/task06/FormValidator.java
@@ -0,0 +1,50 @@
+package homeworks.profileWork.chapter01.task06;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class FormValidator {
+    public static void checkName(String str){
+        if ((str.length() < 2) && (str.length() > 20)){
+            throw new IllegalArgumentException("Username is too long");
+        }
+    }
+
+    public static void checkBirthdate(String str) throws IllegalArgumentException{
+        LocalDate birthdate;
+        try {
+            // Пытаемся распарсить дату из строки
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            birthdate = LocalDate.parse(str, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Неверный формат даты рождения");
+        }
+
+        LocalDate minDate = LocalDate.of(1900, 1, 1);
+        LocalDate maxDate = LocalDate.now();
+        if (birthdate.isBefore(minDate) || birthdate.isAfter(maxDate)){
+            throw new IllegalArgumentException("Дата рождения должна быть не раньше 01.01.1900 и не позже текущей даты");
+        }
+    }
+
+    public static void checkGender(String str){
+        try {
+            Gender gender = Gender.valueOf(str);
+        }catch (IllegalArgumentException e){
+            System.out.println("Некорректное значение пола");
+        }
+    }
+
+    public static boolean checkHeight(String str) {
+        try {
+            double height = Double.parseDouble(str);
+            if (!Double.isFinite(height) || height <= 0) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/homeworks/profileWork/chapter01/task06/Gender.java
@@ -0,0 +1,6 @@
+package homeworks.profileWork.chapter01.task06;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
