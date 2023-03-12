b51a0c41f03713204fd922727f7ecf001bf765e0
Drimmer <drimmer88@gmail.com>
2023-03-12 18:03:00 +0300

    ДЗ_1. Задача 5, исправлено

--- src/homeworks/profileWork/chapter01/task05/Main.java
+++ src/homeworks/profileWork/chapter01/task05/Main.java
@@ -3,18 +3,20 @@ package homeworks.profileWork.chapter01.task05;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-
-        int n = inputN();
-        System.out.println("Успешный ввод!");
-
+    public static void main(String[] args) {        // исправлено
+        try {
+            int n = inputN();
+            System.out.println("Успешный ввод!");
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static int inputN() throws Exception {
         System.out.println("Введите число n, 0 < n < 100");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        if (n < 100 && n > 0) {
+        if (n < 0 || 100 < n) {                     // исправлено
             throw new Exception("Неверный ввод");
         }
         return n;
